8e813b811d092ea5e84349429c23769bd95cee7f
Chandra Shekhar Kondaveni <chandrak@ispace.com>
2020-01-25 12:28:09 +0530

    slot-deployment

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  var model = {
+title: 'azure',
+message: process.env.MESSAGE || "This is Development"
+  };
+ 
   res.render('index', { title: 'Express' });
 });
 
